6499cf010085e7b676136c4ad4e2ea4ae31a5c8d
JiangZhe <1045037872@qq.com>
2020-04-01 19:10:01 +0800

    测试jenkins调用本地代码

--- src/test/java/com/jiang/day3/pratice.java
+++ src/test/java/com/jiang/day3/pratice.java
@@ -1,9 +1,52 @@
 package com.jiang.day3;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.*;
+
 public class pratice {
-    public static void main(String[] args) {
-        long time = System.currentTimeMillis();
-        String t = (""+time).substring(0,11);
-        System.out.println(t);
+    @BeforeTest
+    public void test(){
+        System.out.println("++++++++++++++++++++++++++++++");
     }
+    @Test
+    public void test_01(){
+        System.out.println("hello word");
+    }
+    @AfterTest
+    public void AterTest(){
+        System.out.println("================================");
+    }
+
+//
+//    public void creade_file() throws IOException {
+//        File file = new File("F:/IdeaProjects/File/filename.txt");
+//        if (!file.exists()) {
+//            file.createNewFile();
+//        }
+//        FileWriter fw = null;
+//        try {
+//            fw = new FileWriter(file);
+//            fw.append("卡卡卡卡卡");
+//            fw.flush();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (null != fw)
+//                fw.close();
+//        }
+//    }
+//
+//    public void read_file() throws IOException {
+//        File file = new File("F:/IdeaProjects/File/filename.txt");
+//        FileReader fileReader = new FileReader(file);
+//        BufferedReader bf = new BufferedReader(fileReader);
+//        String str;
+//        while ((str = bf.readLine()) != null) {
+//            System.out.println(str);
+//        }
+//        fileReader.close();
+//    }
 }
